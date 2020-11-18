df86396179d3d9c2c178d5c89a92850967ef9ef3
EgorBedokurov <73665129+EgorBedokurov@users.noreply.github.com>
2020-11-18 23:20:14 +0200

    Add files via upload

--- lesson_6.3.py
+++ lesson_6.3.py
@@ -3,11 +3,9 @@
 
 any_list = ['a', 'b', 'c', 'd', 'e']
 keys = []
-#
-# for i in range(len(any_list)):
-#     i += 1
-#     keys.append(i)
-#
-# print(dict(zip(keys, any_list)))
 
-print(f'(for i in range(len(any_list)), i +=1 keys.append(i)))
+for i in range(len(any_list)):
+    i += 1
+    keys.append(i)
+
+print(dict(zip(keys, any_list)))
